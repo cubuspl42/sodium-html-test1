5cb09b74c86ec19d65929d5b6cebbd56c2dc7a5b
cubuspl42 <cubuspl42@gmail.com>
2020-07-16 21:50:31 +0200

    Simplify Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,41 @@
-const webpack = require('webpack');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const nodeEnv = process.env.NODE_ENV || 'development';
-const isProd = nodeEnv === 'production';
 
-const plugins = [
-  new webpack.DefinePlugin({
-    'process.env': {
-      NODE_ENV: JSON.stringify(nodeEnv)
-    }
-  }),
-  new HtmlWebpackPlugin({
-    title: 'Typescript Webpack Starter',
-    template: '!!ejs-loader!src/index.html'
-  }),
-  new webpack.LoaderOptionsPlugin({
-    options: {
-      tslint: {
-        emitErrors: true,
-        failOnHint: true
-      }
-    }
-  })
-];
-
-var config = {
-  devtool: 'inline-source-map',
-  context: path.resolve('./src'),
-  entry: {
-    app: './index.ts'
-  },
-  output: {
-    path: path.resolve('./dist'),
-    filename: '[name].bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        enforce: 'pre',
-        test: /\.tsx?$/,
-        exclude: [/\/node_modules\//],
-        use: ['awesome-typescript-loader', 'source-map-loader']
-      },
-      !isProd
-        ? {
-            test: /\.(js|ts)$/,
-            loader: 'istanbul-instrumenter-loader',
-            exclude: [/\/node_modules\//],
-            query: {
-              esModules: true
-            }
-          }
-        : null,
-      { test: /\.html$/, loader: 'html-loader' },
-      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] }
-    ].filter(Boolean)
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  plugins: plugins,
-  devServer: {
-    contentBase: path.join(__dirname, 'dist/'),
-    compress: true,
-    port: 3000,
-    hot: true
-  }
+module.exports = {
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.js$/,
+                enforce: 'pre',
+                use: ['source-map-loader'],
+                exclude: [
+                    path.resolve(__dirname, './node_modules/typescript-collections'),
+                ]
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'src'),
+        compress: true,
+        port: 3005,
+        historyApiFallback: true,
+        publicPath: '/',
+    },
 };
-
-module.exports = config;
